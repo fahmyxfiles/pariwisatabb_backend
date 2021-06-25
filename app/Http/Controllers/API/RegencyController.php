339470f4252400a40f3e77d0bf86e6bdcdadf941
fahmyxfiles <fahmy.xfiles@gmail.com>
2021-06-25 08:46:54 +0800

    commit_main

--- app/Http/Controllers/API/RegencyController.php
+++ app/Http/Controllers/API/RegencyController.php
@@ -17,7 +17,7 @@ use Intervention\Image\Facades\Image;
 
 class RegencyController extends BaseController
 {
-    const ITEM_PER_PAGE = 15;
+    const ITEM_PER_PAGE = 6;
     /**
      * Display a listing of the resource.
      *
--- app/Http/Resources/Province.php
+++ app/Http/Resources/Province.php
@@ -14,9 +14,6 @@ class Province extends JsonResource
      */
     public function toArray($request)
     {
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
-        ];
+        return parent::toArray($request);
     }
 }
