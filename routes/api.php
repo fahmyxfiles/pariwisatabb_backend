be712b93d04ae969ec88de9522de74df6c69370b
fahmyxfiles <fahmy.xfiles@gmail.com>
2021-06-27 01:32:14 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -45,5 +45,5 @@ Route::middleware('auth:api')->group( function () {
     Route::resource('hotel', HotelController::class);
 
     Route::resource('facility', FacilityController::class);
-    Route::get('facility/getAvailableCategories/{type?}', [FacilityController::class, 'getAvailableCategories']);
+    Route::get('facility/getAvailableCategories{/type?}', [FacilityController::class, 'getAvailableCategories']);
 });
