c4a7ad14f486a185f99d683a2a4126a62540caf6
fahmyxfiles <fahmy.xfiles@gmail.com>
2021-07-01 15:05:47 +0800

    Update HotelRoom.php

--- app/Http/Resources/HotelRoom.php
+++ app/Http/Resources/HotelRoom.php
@@ -14,6 +14,6 @@ class HotelRoom extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return array_merge(parent::toArray($request), ['value' => 'Y']);
     }
 }
