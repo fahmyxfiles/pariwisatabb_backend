ee7b25c27f9bae7899633588a4f1a1c73bf44acb
fahmyxfiles <fahmy.xfiles@gmail.com>
2021-07-02 01:37:32 +0800

    Update HotelImageController.php

--- app/Http/Controllers/API/HotelImageController.php
+++ app/Http/Controllers/API/HotelImageController.php
@@ -47,7 +47,7 @@ class HotelImageController extends BaseController
    
         $validator = Validator::make($input, [
             'hotel_id' => 'required|exists:hotels,id',
-            'hotel_room_id' => 'exists:hotels,id',
+            'hotel_room_id' => 'nullable|exists:hotels,id',
             'name' => 'required',
             'type' => 'required',
             'file' => 'required|image'
@@ -113,7 +113,7 @@ class HotelImageController extends BaseController
    
         $validator = Validator::make($input, [
             'hotel_id' => 'required|exists:hotels,id',
-            'hotel_room_id' => 'exists:hotels,id',
+            'hotel_room_id' => 'nullable|exists:hotels,id',
             'name' => 'required',
             'type' => 'required',
             'file' => 'image'
